472d974609a3e30abfabc4e2163481c1641290c6
Khaled Nabil <60897225+khalednabil919@users.noreply.github.com>
2024-06-30 17:22:33 +0300

    Add project files.

---
+++ ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace SignalRDotNetFrameWork
+{
+    [HubName("chat")]
+    public class ChatHub:Hub
+    {
+        public void sendMessage(string name,string message)
+        {
+            Clients.All.newMessage(name, message); //RPC
+        }
+        public void joinGroup(string gname,string name)
+        {
+            Groups.Add(Context.ConnectionId, gname);
+            Clients.OthersInGroup(gname).newMember(name, gname);
+        }
+        public void sendGroup(string name, string gname , string message)
+        {
+            Clients.Group(gname).sendToAllMemberInGroup(name , gname, message);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SignalRDotNetFrameWork")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LinkDev")]
+[assembly: AssemblyProduct("SignalRDotNetFrameWork")]
+[assembly: AssemblyCopyright("Copyright © LinkDev 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba29d8bd-45a5-44a9-994e-1c17f1f1df0b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
